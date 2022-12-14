ce36289705ab0c75e4c88496aebe4cf6435093c6
7FrogTW <jjyyg1123@gmail.com>
2022-12-14 11:45:12 +0800

    fix: stop zsh launching after installtion

--- .zshrc
+++ .zshrc
@@ -9,13 +9,13 @@ fi
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/Users/jackyyin/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_THEME="random"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -113,7 +113,6 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
 alias kb="kubectl"
-alias vim='nvim'
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- init.sh
+++ init.sh
@@ -3,14 +3,19 @@
 sudo apt-get update -y
 sudo apt-get install -y git zsh tmux build-essential fzf vim vim-gtk cmake autojump silversearcher-ag
 
-# ZSH
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-cp .zshrc ~
 # VIM
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~
 vim +PluginInstall +qa ~/.vimrc
+
 # TMUX
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~
+
+#ZSH
+wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O zsh_install.sh
+sed -i 's/exec zsh -l//g' zsh_install.sh
+sh zsh_install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cp .zshrc ~
+exec zsh -l
